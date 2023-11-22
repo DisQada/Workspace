b168faa5858d22f405ab0312201ff0642fee58a1
The Alpha <contact@nabilalsaiad.com>
2023-11-22 12:18:02 +0800

    chore(config): ignore non-template config files
    Non-template config files will be deleted and ignored, so, all the related things will be deleted
    too

--- reset.js
+++
@@ -1,6 +0,0 @@
-const { writeFileSync } = require("fs");
-const { resolve } = require("path");
-
-writeFileSync(resolve("./config/tsconfig.json"), "{}");
-writeFileSync(resolve("./config/tsconfig.doc.json"), "{}");
-writeFileSync(resolve("./config/typedoc.json"), "{}");
