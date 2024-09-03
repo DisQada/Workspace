d8030bcb71714bbd6e4546dc517de01fd706a568
Assayyaad <hi@thealpha.pro>
2024-09-03 09:44:28 +0800

    test: إعادة كتابة ملف إختبار التنظيف

--- test/clean.test.js
+++ test/clean.test.js
@@ -1,74 +1,122 @@
 import { equal } from 'assert/strict'
 import { cleanFile } from '../cli/func/clean.js'
 
-const fileData = `import { something } from "example/path";
-
-export declare type Type1 = import("example/path").string | import("example/path").number;
-
-export type Type2 = {
-    /**
-     * pretend to be a useful comment
-     */
-    some?: import("../types/example/path").SomeType;
-};
-
-export enum Type3 = {
-    other: import("../types/example/path").OtherType;
-};
-
-export function (...args: number[][], ...args2: Interface[Key][], ...args3: string[][][]) :
-    | Promise<import("example/path").string | void>
-    | string
-    | void { }
-
-export type string = typeof import("./example/path");
-
-import type { string } from "example/path";
-export { string };
-
-export type exports = number
-
-export = something;
-`
-
-const cleanedFile = `import type { string, number } from "example/path";
-import type { SomeType, OtherType } from "../types/example/path";
-
-import { something } from "example/path";
-
-export declare type Type1 = string | number;
-
-export declare type Type2 = {
-    /**
-     * pretend to be a useful comment
-     */
-    some?: SomeType;
-};
-
-export declare enum Type3 = {
-    other: OtherType;
-};
-
-export declare function (...args: number[], ...args2: Interface[Key], ...args3: string[][]) :
-    | Promise<string | void>
-    | string
-    | void { }
-
-export declare type string = typeof import("./example/path");
-
-import type { string } from "example/path";
-export { string };
-
-export declare type exports = number
-
-export default something;
-`
-
 describe('clean', function () {
   describe('cleanFile()', function () {
-    it('should clean it properly', function () {
-      const newFile = cleanFile(fileData)
-      equal(newFile, cleanedFile)
+    it('should convert export to default export', function () {
+      const a = cleanFile('export = example;')
+      equal(a, 'export default example;')
+    })
+
+    it('should keep export as is', function () {
+      let a = cleanFile('export * as Types from "./file/path.js";')
+      equal(a, 'export * as Types from "./file/path.js";')
+
+      a = cleanFile('export { example };')
+      equal(a, 'export { example };')
+
+      a = cleanFile('import type { example } from "./file/path.js";')
+      equal(a, 'import type { example } from "./file/path.js";')
+    })
+
+    it('should convert export type to declare export type', function () {
+      let a = cleanFile('export type exports = example;')
+      equal(a, 'export declare type exports = example;')
+
+      a = cleanFile('export type example = typeof import("./file/path.js");')
+      equal(a, 'export declare type example = typeof import("./file/path.js");')
+    })
+
+    it('should convert export function to declare export function', function () {
+      const a =
+        cleanFile(`export function (...args: example[][], ...args2: example[example][], ...args3: example[][][]) :
+    | Promise<import("file/path.js").example | void>
+    | example
+    | void { }`)
+      equal(
+        a,
+        `import type { example } from "file/path.js";
+
+export declare function (...args: example[], ...args2: example[example], ...args3: example[][]) :
+    | Promise<example | void>
+    | example
+    | void { }`
+      )
+    })
+
+    it('should convert export enum to declare export enum', function () {
+      const a = cleanFile(`export enum Example = {
+  other: import("../file/path.js").OtherExample;
+};`)
+      equal(
+        a,
+        `import type { OtherExample } from "../file/path.js";
+
+export declare enum Example = {
+  other: OtherExample;
+};`
+      )
+    })
+
+    it('should convert export type to declare export type with comment', function () {
+      const a = cleanFile(
+        `export type Example = {
+  /**
+   * pretend to be a useful comment
+   */
+  some?: import("../file/path.js").SomeExample;
+};`
+      )
+      equal(
+        a,
+        `import type { SomeExample } from "../file/path.js";
+
+export declare type Example = {
+  /**
+   * pretend to be a useful comment
+   */
+  some?: SomeExample;
+};`
+      )
+    })
+
+    it('should convert export type to declare export type with import', function () {
+      const a = cleanFile(
+        'export declare type Example = import("./file/path").example1 | import("./file/path").example2;'
+      )
+      equal(
+        a,
+        `import type { example1, example2 } from "./file/path";
+
+export declare type Example = example1 | example2;`
+      )
+    })
+
+    it('should keep import as is', function () {
+      const a = cleanFile('import { example } from "./file/path";')
+      equal(a, 'import { example } from "./file/path";')
+    })
+
+    it('should convert import to import type', function () {
+      const a = cleanFile(`const e1 = import("./file/path.js").Example1;
+const e2 = import("./file/path.js").Example2;`)
+      equal(
+        a,
+        `import type { Example1, Example2 } from "./file/path.js";
+
+const e1 = Example1;
+const e2 = Example2;`
+      )
+    })
+
+    it('should remove typedef', function () {
+      const a = cleanFile('/** @typedef {Example} Example */')
+      equal(a, '')
+    })
+
+    it('should remove export type', function () {
+      const a = cleanFile('export type Example = Example;')
+      equal(a, '')
     })
 
     it('should return it as is', function () {
